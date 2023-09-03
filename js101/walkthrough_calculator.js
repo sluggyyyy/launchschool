42969857f49ac03056bfdb2aa73ad007561ba44d
Zachary Morgan <zacharymorgannyc@gmail.com>
2023-09-02 22:46:03 -0400

    Coursework for the night

--- js101/walkthrough_calculator.js
+++ js101/walkthrough_calculator.js
@@ -1,33 +1,62 @@
 const readline = require('readline-sync');
 
+// Standardizer for prompts asking user for input
+let prompt = (message) => {
+  console.log(`=> ${message}`);
+};
+
+// Function for handling invalid inputs
+let invalidNumber = (number) => {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+};
+
 console.log('Welcome to Calculator!\n');
 
-// Ask the user for the first number.
-let number1 = readline.question("What's the first number? ");
+// Ask the user for the first number with error handling.
+prompt("What's the first number? ");
+let number1 = readline.question();
 // console.log(number1);
 
-// Ask the user for the second number.
-let number2 = readline.question("What's the second number? ");
-// console.log(`${number1} ${number2}`);
+while (invalidNumber(number1)) {
+  prompt("Hmm... that doesn't look like a valid number. Please enter a new value:");
+  number1 = readline.question();
+}
 
-// Ask the user for an operation to perform 
-let operation = readline.question("\nWhat operation would you like to perform?\n\n  1) Add 2) Subtract 3) Multiply 4) Divide: ");
+// Ask the user for the second number with error handling.
+prompt("What's the second number? ");
+let number2 = readline.question();
+// console.log(`${number1} ${number2}`);
+while (invalidNumber(number2)) {
+  prompt("Hmm... that doesn't look like a valid number. Please enter a new value:");
+  number2 = readline.question();
+}
+
+// Ask the user for an operation to perform with error handling.
+prompt("What operation would you like to perform?\n\n  1) Add 2) Subtract 3) Multiply 4) Divide:\n");
+let operation = readline.question();
+// Error handling for operation specifically.
+
+while (!['1', '2', '3', '4'].includes(operation)) {
+  prompt('Must choose 1, 2, 3 or 4');
+  operation = readline.question();
+}
 
 // Perform the operation on the two numbers.
 // Print the result to the terminal.
 let output;
-if (operation === '1') {
+switch (operation) {
+  case '1':
     output = Number(number1) + Number(number2);
-    console.log(`The result is: ${output}`);
-} else if (operation === '2') {
+    break;
+  case '2':
     output = Number(number1) - Number(number2);
-    console.log(`The result is: ${output}`);
-} else if (operation === '3') {
+    break;
+  case '3':
     output = Number(number1) * Number(number2);
-    console.log(`The result is: ${output}`);
-} else if (operation === '4') {
+    break;
+  case '4':
     output = Number(number1) / Number(number2);
-    console.log(`The result is: ${output}`);
-} else {
-    console.log('\n Please try again and enter a valid operator selection...')
+    break;
 }
+
+console.log(output);
